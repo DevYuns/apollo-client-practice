8faa0e3d030f36a8c25e5670ea98274d7b8bfb01
DevYuns <ydhun93@gmail.com>
2021-02-22 21:30:27 +0900

    Set React-Router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,16 @@
+import {Route, HashRouter as Router} from 'react-router-dom';
+
+import {Detail} from '../routes/Detail';
+import {Home} from '../routes/Home';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/:id' component={Detail} />
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/routes/Detail.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react';
+
+export const Detail: FC = () => {
+  return (
+    <div>
+      "Detail"
+    </div>
+  );
+}
